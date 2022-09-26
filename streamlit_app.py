ffd98ece71b461e53a634e90189afbe311b96456
Alex Robillard, PhD <52674718+RobillardA@users.noreply.github.com>
2022-09-26 11:57:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,15 +21,15 @@ def main():
        
     if image:
             
-        for image in images:
-            with Image.open(image) as img:
-            image_data = img.imread()
-            st.image(image_data, use_column_width=True)
+    for image in images:
+        with Image.open(image) as img:
+        image_data = img.imread()
+        st.image(image_data, use_column_width=True)
 
-            prediction = model.predict(image_data)
+        prediction = model.predict(image_data)
         
-            pred_chart = predictions_to_chart(prediction, classes = model.dls.vocab)
-            st.altair_chart(pred_chart, use_container_width=True)
+        pred_chart = predictions_to_chart(prediction, classes = model.dls.vocab)
+        st.altair_chart(pred_chart, use_container_width=True)
 
 def predictions_to_chart(prediction, classes):
     pred_rows = []
