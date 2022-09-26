01b89e2bffefd6dc22f316944f07c2c170758f27
Alex Robillard, PhD <52674718+RobillardA@users.noreply.github.com>
2022-09-26 15:37:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,15 +19,14 @@ def main():
     st.markdown("Dive Image for Classification.")
     image = st.file_uploader("", IMAGE_TYPES, accept_multiple_files = True)
     
-#    if image:
-     for image in images:
-        with Image.open(image) as img:
-            image_data = img.imread()
-            
-#               st.image(image_data, use_column_width=True)   
-            prediction = model.predict(image_data)
-        pred_df = predictions_to_df(prediction, classes = model.dls.vocab)
-        st.dataframe(top_probs, use_container_width=True)
+    if image:
+        for images in image:
+            with Image.open(images) as img:
+                image_data = img.imread()
+                st.image(image_data, use_column_width=True)
+                prediction = model.predict(image_data)
+                pred_df = predictions_to_df(prediction, classes = model.dls.vocab)
+                st.dataframe(top_probs, use_container_width=True)
 
 def predictions_to_df(prediction, classes):
     pred_rows = []
